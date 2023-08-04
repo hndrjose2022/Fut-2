b93accd38d6321c1d981d9826c8dc86e764e6a1e
hndrjose2022 <jmendoza.hndr@gmail.com>
2023-08-03 21:27:49 -0600

    cambio a mysql

--- global/environment.ts
+++ global/environment.ts
@@ -4,17 +4,17 @@ export const SERVER_PORT: number = Number( process.env.PORT ) || 5800;
 
 
 
-export const connection = mysql.createConnection({
-    host: 'localhost', // Localhost,  35.202.222.43
-    user: 'root', // root
-    password: 'Caramel-2020', // Ceutec-19, systemas
-    database: 'jugadores', //
-});
-
-
 // export const connection = mysql.createConnection({
-//     host: 'database-jugadores.cqezygy3ehcy.us-east-2.rds.amazonaws.com', // Localhost,  35.202.222.43
-//     user: 'admin', // root
-//     password: 'Caramel-2020', // Ceutec-19, systemas
-//     database: 'jugador', //
+//     host: 'localhost', // Localhost,  35.202.222.43
+//     user: 'root', // root
+//     password: 'Caramel-2020', // Ceutec-19, systemas 
+//     database: 'jugadores' //
 // });
+
+
+export const connection = mysql.createConnection({
+    host: 'jugadores.cqezygy3ehcy.us-east-2.rds.amazonaws.com', // Localhost,  35.202.222.43
+    user: 'admin', // root
+    password: 'Caramel-2020', // Ceutec-19, systemas
+    database: 'jugadores' //
+});
